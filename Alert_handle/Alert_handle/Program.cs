3409a5c3d9f65ffbb73601a08fea4cf6c31c4242
Natarajan <theultimate.dreamer@gmail.com>
2016-01-30 20:20:05 +0530

    Added Alert project

---
+++ Alert_handle/Alert_handle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.UI;
+
+namespace Alert_handle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver;
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://www.javascriptsandmore.com/javascript-alert-box.html");
+            IAlert alert = driver.SwitchTo().Alert();
+            Console.WriteLine(alert.Text);
+            alert.Accept();
+            IAlert next_alert = driver.SwitchTo().Alert();
+            Console.WriteLine(next_alert.Text);
+            next_alert.Accept();
+            driver.Quit();
+            Console.Read();
+        }
+    }
+}
